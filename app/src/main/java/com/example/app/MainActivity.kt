9364c10cb97a48700148c2c0e2af40a893b40959
7c00 <i@7c00.cc>
2021-12-15 15:45:44 +0800

    Update

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -3,14 +3,33 @@ package com.example.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setContent { Main() }
+  }
+}
 
-    setContent {
-      Text(text = "Hello world!")
+@Composable
+fun Main() {
+  AppTheme {
+    Surface(color = MaterialTheme.colors.background) {
+      Column {
+        Text(text = "Hello world!")
+      }
     }
   }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainPreview() {
+  Main()
 }
---
+++ app/src/main/java/com/example/app/Theme.kt
@@ -0,0 +1,22 @@
+package com.example.app
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColors = darkColors()
+private val LightColors = lightColors()
+
+@Composable
+fun AppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+  val colors = if (darkTheme) {
+    DarkColors
+  } else {
+    LightColors
+  }
+
+  MaterialTheme(colors = colors, content = content)
+}
+
